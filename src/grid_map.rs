30e940fcdbb75cf06763c1bb2e63b9f832a16f5d
Salah <rsasaki0109@gmail.com>
2024-03-11 00:26:30 +0900

    add grid map environment for plannar 2d navigation

---
+++ src/grid_map.rs
@@ -0,0 +1,27 @@
+// grid map definition
+// author:Salah Eddine Ghamri (s.ghamri)
+
+use std::ops::Deref;
+extern crate nalgebra as na;
+
+pub struct Map{
+    _grid: na::DMatrix<i32>
+}
+
+impl Map {
+    pub fn new(original_matrix: na::DMatrix<i32>, scale: usize) -> Result<Self, &'static str> {
+        if scale < 1 {
+            return Err("scale must be >= 1");
+        }
+        let _grid = original_matrix.kronecker(&na::DMatrix::<i32>::repeat(scale, scale, 1));
+        Ok(Self {_grid})
+    }
+}
+
+impl Deref for Map {
+    type Target = na::DMatrix<i32>;
+
+    fn deref(&self) -> &Self::Target {
+        &self._grid
+    }
+}
