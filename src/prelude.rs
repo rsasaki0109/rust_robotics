02dd46894ed6d4b10b30374809e658fbd439af23
Ryohei Sasaki <rsasaki0109@gmail.com>
2026-03-11 20:46:34 +0900

    Expand stable localization APIs for UKF and PF

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,7 +3,11 @@
 pub use crate::common::{
     ControlInput, Obstacles, Path2D, Point2D, Pose2D, RoboticsError, RoboticsResult, State2D,
 };
-pub use crate::localization::{EKFConfig, EKFControl, EKFLocalizer, EKFMeasurement, EKFState};
+pub use crate::localization::{
+    EKFConfig, EKFControl, EKFLocalizer, EKFMeasurement, EKFState, PFControl, PFMeasurement,
+    PFState, Particle, ParticleFilterConfig, ParticleFilterLocalizer, UKFConfig, UKFControl,
+    UKFLocalizer, UKFMeasurement, UKFParams, UKFState, UKFWeights,
+};
 pub use crate::path_planning::{
     AStarConfig, AStarPlanner, DWAConfig, DWAControl, DWAPlanner, DWAState, DWATrajectory,
     JPSConfig, JPSPlanner, ThetaStarConfig, ThetaStarPlanner,
